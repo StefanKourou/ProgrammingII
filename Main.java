60673a1d18362b40a92b812040ca07c56ab35e96
HliasMpGH <t8210226@aueb.gr>
2022-11-05 10:49:25 +0200

    Revert "Delete Main.java"
    This reverts commit 2fe7e853bcc24835e6f5736229b1d6064f12c2b5.

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        SocialMain obj = new SocialMain();
+        while (true) {
+            obj.createUser();
+            
+        }
+
+    }
+}
