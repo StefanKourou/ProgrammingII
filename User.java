43bbb7f26aebaaa48e863fa86821fdce8f644cab
HliasMpGH <t8210226@aueb.gr>
2022-11-17 00:49:55 +0200

    Update User.java

--- User.java
+++ User.java
@@ -4,11 +4,19 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 import java.sql.Connection;
+import java.util.Random;
+
 
 public class User {
+    Connection conn;
     Scanner in = new Scanner(System.in);
+    Random r = new Random();
 
-    public ResultSet CheckExistingUser(Connection conn, String name) {
+    public User(Connection conn) {
+        this.conn = conn;
+    }
+
+    public ResultSet CheckExistingUser(String name) {
         String sql = "SELECT name FROM USERS WHERE NAME="+name;
         ResultSet  result = null;
         try {    
@@ -20,17 +28,17 @@ public class User {
         return result;       
     }
 
-    public String RegisterNewUser(Connection conn) {
+    public void RegisterNewUser() {
         while (true) {
             String name = in.nextLine();
             try { 
-                if (CheckExistingUser(conn, name).getString("name").equals(name)) {
+                if (CheckExistingUser(name).getString("name").equals(name)) {
                         System.out.println("User with username: " + name + " already exists!");
                         System.out.println("Please choose a different UserName: ");
                     } else {
                         String pw = CreatePw();
                         String email = CreateEmail();
-                        InserUserInDB(conn, name, pw, email, "login_time");
+                        InserUserInDB(name, pw, email, "login_time");
 
                     }
         } catch (SQLException e) {
@@ -40,17 +48,41 @@ public class User {
 }
     
     public String CreatePw() {
-        System.out.print("Please choose a valid Password: ");        
+        System.out.print("Please choose a valid Password, or type 'help' to get an auto-generated one: ");      
         while (true) {
           String  pw = in.nextLine();
+          if (!pw.equals("help")) {
             if (pw.length() <= 5 || (!pw.contains("!") && !pw.contains("@") && !pw.contains("#") && !pw.contains("$"))) {
                 System.out.print("Your password is really weak! Please choose a different one: ");
             } else {
                 System.out.println("Password has been successfully created!");
                 return pw;
             }
+          } else {
+            String an = "0123456789abcdefghijklmnopqrstuvwxyz!@#$";
+            while(true) {
+                StringBuilder agpw = new StringBuilder();
+                int len = r.nextInt(10) + 6;
+                int s=1;
+                for (int i = 0; i < len; i++) {
+                    s = r.nextInt(39)+1;
+                    agpw.append(an.substring(s,s+1));
+                }
+                System.out.println("Your generated password is: "+agpw+" , good choice!");
+                System.out.print("Want to generate a new one? (Y/N) : ");
+                String x = in.nextLine();
+                if (x.equals("Y")) {
+                    continue;
+                } else {
+                    return agpw.toString();
+                }
+            }
+            }
         }
     }
+    
+    
+    
     public String CreateEmail() {
         System.out.print("Please enter your email: ");
         while (true) {
@@ -66,7 +98,7 @@ public class User {
         
     }
         
-        public void InserUserInDB(Connection conn, String name, String pw, String email, String login_time) {
+        public void InserUserInDB(String name, String pw, String email, String login_time) {
             String sql = "INSERT INTO USERS(Username, Password, Email, LoginTime) VALUES(?, ?, ?, ?)";
             try {
                 PreparedStatement ps = conn.prepareStatement(sql);
@@ -75,7 +107,6 @@ public class User {
                 ps.setString(3, email);
                 ps.setString(4, login_time);
                 ps.executeUpdate();
-    
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -83,3 +114,5 @@ public class User {
     
 
 }
+
+
