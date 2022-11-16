004d641812c3bc9046c2db078cb8b58db8fecbd1
HliasMpGH <t8210226@aueb.gr>
2022-11-17 01:40:59 +0200

    Update User.java

--- User.java
+++ User.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import java.sql.Connection;
 import java.util.Random;
 
-
 public class User {
     Connection conn;
     Scanner in = new Scanner(System.in);
@@ -17,102 +16,98 @@ public class User {
     }
 
     public ResultSet CheckExistingUser(String name) {
-        String sql = "SELECT name FROM USERS WHERE NAME="+name;
-        ResultSet  result = null;
-        try {    
+        String sql = "SELECT name FROM USERS WHERE NAME=" + name;
+        ResultSet result = null;
+        try {
             Statement stm = conn.createStatement();
-            result = stm.executeQuery(sql);    
+            result = stm.executeQuery(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return result;       
+        return result;
     }
 
     public void RegisterNewUser() {
         while (true) {
             String name = in.nextLine();
-            try { 
+            try {
                 if (CheckExistingUser(name).getString("name").equals(name)) {
-                        System.out.println("User with username: " + name + " already exists!");
-                        System.out.println("Please choose a different UserName: ");
-                    } else {
-                        String pw = CreatePw();
-                        String email = CreateEmail();
-                        InserUserInDB(name, pw, email, "login_time");
-
-                    }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-}
-    
-    public String CreatePw() {
-        System.out.print("Please choose a valid Password, or type 'help' to get an auto-generated one: ");      
-        while (true) {
-          String  pw = in.nextLine();
-          if (!pw.equals("help")) {
-            if (pw.length() <= 5 || (!pw.contains("!") && !pw.contains("@") && !pw.contains("#") && !pw.contains("$"))) {
-                System.out.print("Your password is really weak! Please choose a different one: ");
-            } else {
-                System.out.println("Password has been successfully created!");
-                return pw;
-            }
-          } else {
-            String an = "0123456789abcdefghijklmnopqrstuvwxyz!@#$";
-            while(true) {
-                StringBuilder agpw = new StringBuilder();
-                int len = r.nextInt(10) + 6;
-                int s=1;
-                for (int i = 0; i < len; i++) {
-                    s = r.nextInt(39)+1;
-                    agpw.append(an.substring(s,s+1));
-                }
-                System.out.println("Your generated password is: "+agpw+" , good choice!");
-                System.out.print("Want to generate a new one? (Y/N) : ");
-                String x = in.nextLine();
-                if (x.equals("Y")) {
-                    continue;
+                    System.out.println("User with username: " + name + " already exists!");
+                    System.out.println("Please choose a different UserName: ");
                 } else {
-                    return agpw.toString();
+                    String pw = CreatePw();
+                    String email = CreateEmail();
+                    InserUserInDB(name, pw, email, "login_time");
+
                 }
-            }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
     }
-    
-    
-    
+
+    public String CreatePw() {
+        System.out.print("Please choose a valid Password, or type 'help' to get an auto-generated one: ");
+        while (true) {
+            String pw = in.nextLine();
+            if (!pw.equals("help")) {
+                if (pw.length() <= 5
+                        || (!pw.contains("!") && !pw.contains("@") && !pw.contains("#") && !pw.contains("$"))) {
+                    System.out.print("Your password is really weak! Please choose a different one: ");
+                } else {
+                    System.out.println("Password has been successfully created!");
+                    return pw;
+                }
+            } else {
+                String an = "0123456789abcdefghijklmnopqrstuvwxyz!@#$";
+                while (true) {
+                    StringBuilder agpw = new StringBuilder();
+                    int len = r.nextInt(10) + 6;
+                    int s = 1;
+                    for (int i = 0; i < len; i++) {
+                        s = r.nextInt(39) + 1;
+                        agpw.append(an.substring(s, s + 1));
+                    }
+                    System.out.println("Your generated password is: " + agpw + " , good choice!");
+                    System.out.print("Want to generate a new one? (Y/N) : ");
+                    String x = in.nextLine();
+                    if (x.equals("Y")) {
+                        continue;
+                    } else {
+                        return agpw.toString();
+                    }
+                }
+            }
+        }
+    }
+
     public String CreateEmail() {
         System.out.print("Please enter your email: ");
         while (true) {
             String email = in.nextLine();
             if (email.contains("@") && ((email.contains(".com") || email.contains(".gr")))) {
-                    System.out.println("Valid email sucessfully created!");
-                    return email;
+                System.out.println("Valid email sucessfully created!");
+                return email;
             } else {
                 System.out.println("Invalid email!");
                 System.out.print("Please enter a valid email: ");
             }
         }
-        
+
     }
-        
-        public void InserUserInDB(String name, String pw, String email, String login_time) {
-            String sql = "INSERT INTO USERS(Username, Password, Email, LoginTime) VALUES(?, ?, ?, ?)";
-            try {
-                PreparedStatement ps = conn.prepareStatement(sql);
-                ps.setString(1, name);
-                ps.setString(2, pw);
-                ps.setString(3, email);
-                ps.setString(4, login_time);
-                ps.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }    
-    
+
+    public void InserUserInDB(String name, String pw, String email, String login_time) {
+        String sql = "INSERT INTO USERS(Username, Password, Email, LoginTime) VALUES(?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, name);
+            ps.setString(2, pw);
+            ps.setString(3, email);
+            ps.setString(4, login_time);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
-
-
