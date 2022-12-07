171bf5d8bad2c6957437df3e40d44b8ca8af7e29
HliasMpGH <t8210226@aueb.gr>
2022-12-07 10:24:13 +0200

    f

--- Progr2omadiki/src/main/java/core/Connect.java
+++ Progr2omadiki/src/main/java/core/Connect.java
@@ -1,8 +1,9 @@
 //package core;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+import java.sql.*;
 
 public class Connect {
 
