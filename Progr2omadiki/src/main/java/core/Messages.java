782e0f6e6ece9849f4f12ac35eb14fadc200462c
GTsak314 <tsakalosgeorgios@gmail.com>
2022-12-11 09:31:50 +0200

    Ελεγχος Messages

--- Progr2omadiki/src/main/java/core/Messages.java
+++ Progr2omadiki/src/main/java/core/Messages.java
@@ -6,11 +6,11 @@ public class Messages {
 
 	public void showMessage() {
 	        String msgGroups =
-	        "SELECT MsgText, MsgUsername;
-			FROM Messages;
-			WHERE GroupID=2 //EDW THA PAIRNOUME INPUT APO TH MSGGROUPS!!!;
-			ORDER BY MsgCreationTime ASC;
-			LIMIT 10;";
+	        "SELECT MsgText, MsgUsername 
+			FROM Messages 
+			WHERE GroupID=2 //EDW THA PAIRNOUME INPUT APO TH MSGGROUPS!!!
+			ORDER BY MsgCreationTime ASC 
+			LIMIT 10;"
 
 	        try (Connection conn = this.connect(); // Hlias: tha exeis to conn obj apo thn Connect class, den xreiazetai na to dhmiourgeis 
 	             Statement stmt  = conn.createStatement();
@@ -19,7 +19,9 @@ public class Messages {
 	        // loop through the result set
 	        while (queryresult.next()) {
 	        	System.out.println(queryresult.getString("MsgText") + "\t" +
-	            			   queryresult.getString("MsgUsername"));
+	        			queryresult.getString("MsgID") + "\t" +
+	        			queryresult.getString("MsgCreationTime") + "\t" +
+	        			queryresult.getString("MsgUsername"));
 	            }
 	        } catch (SQLException e) {
 	        	System.out.println(e.getMessage());
@@ -28,11 +30,9 @@ public class Messages {
 
     	public void createMessage() {
 		        String newMessage =
-		        "INSERT INTO Messages (
-		        Username, GroupID, MsgText, MsgCreationTime)  //USERNAME APO TH LOGIN GROUPID APO TH MESSAGE GROUP
+		        "INSERT INTO Messages (MsgUsername, MsgGroup, MsgText, MsgCreationTime) // USERNAME APO TH LOGIN GROUPID APO TH MESSAGE GROUP \\
 				VALUES (			// Hlias: prepei na pairines os orisma to MsgText
-				‘Spinelis’, 2, ‘ãéá íá äïõìå ðùò öôéá÷íåôáé’, DATETIME('now','localtime')) // pithanotata xrhsimopoihsh ths getTime()
-				";
+				‘Spinelis’, 2, ‘για να δουμε πως φτιαχνεται’, DATETIME('now','localtime'));" // pithanotata xrhsimopoihsh ths getTime()
 
 		        try (Connection conn = this.connect();
 		             Statement stmt  = conn.createStatement();
@@ -40,10 +40,8 @@ public class Messages {
 
 		        // loop through the result set
 		        while (queryresult.next()) {
-		        	System.out.println(queryresult.getString("Username") + "\t" +
-		            			   queryresult.getString("GroupID") + "\t" +
-		            			   queryresult.getBoolean("MsgText") + "\t" +
-		            			   queryresult.getString("MsgCreationTime"));
+		        	System.out.println(queryresult.getString("MsgUsername") + "\t" +
+		            			   queryresult.getBoolean("MsgText") + "\t";
 		            }
 		        } catch (SQLException e) {
 		            System.out.println(e.getMessage());
@@ -53,7 +51,7 @@ public class Messages {
 
     	public void reactionsMessage() {
 		String newReaction =
-		"INSERT INTO Reactions (MsgID, Username, Reaction)
+		"INSERT INTO Reactions (ReMsg, ReUsername, Reaction)
 		 VALUES (10, ‘testuser3’, 1)              //same me panw
 		";
 
@@ -63,8 +61,8 @@ public class Messages {
 
 			// loop through the result set
 			while (queryresult.next()) {
-			        System.out.println(queryresult.getString("MsgID") + "\t" +
-			            		   queryresult.getString("Username") + "\t" +
+			        System.out.println(queryresult.getString("ReMsg") + "\t" +
+			            		   queryresult.getString("ReUsername") + "\t" +
 			            		   queryresult.getBoolean("Reaction"));
 			  	}
 			} catch (SQLException e) {
