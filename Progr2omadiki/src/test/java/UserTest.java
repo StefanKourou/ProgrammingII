548aff7cbe1dc93e0b3a17906628f8bbbebdccd6
HliasMpGH <t8210226@aueb.gr>
2022-12-13 00:38:43 +0200

    Added UserTest

---
+++ Progr2omadiki/src/test/java/UserTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import java.sql.Connection;
+import org.sql.Statement;
+
+public class UserTest {
+
+    @before 
+    void setUp() {
+        Connection conn = Connect.connect(); // establishing connection with the DB
+        Statement stm = conn.createStatement();
+        String sqlstm = "INSERT INTO USERS(Username, Password, Discoverable, Email)" +
+        "VALUES('testcase1', 'testpassword1', 0, 'testemail@gmail.com')"; // inserting a test-user in the DB
+        ResultSet r = stm.executeQuery(sqlstm);
+        assertEquals();
+    }
+
+}
