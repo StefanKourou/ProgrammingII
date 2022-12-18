911441f025f7f1e390e81e7136c63a6cd7525811
HliasMpGH <115589434+HliasMpGH@users.noreply.github.com>
2022-12-18 16:55:03 +0200

    Updated MsgGroups
    isws mia methodos gia add users

--- Progr2omadiki/src/main/java/core/MsgGroups.java
+++ Progr2omadiki/src/main/java/core/MsgGroups.java
@@ -30,18 +30,18 @@ public class MsgGroups extends User {
 		 return conn;
 	}
 
-	public void createMsgGroup(String name) {
+	public void createMsgGroup() {
 		System.out.println("Please insert a name for your MessageGroup");
 		String groupName= input.nextLine();
 		System.out.println("Now insert the group's theme");
 		String groupKeyword = input.nextLine();
 		String sql = "INSERT INTO MsgGroups (MsgGroupName, GroupKeywords, MsgGroupCreator) "
-						+ "VALUES (groupName, groupKeyword, name)";
+						+ "VALUES (?, ?, ?)";
 		try  { 
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 		        pstmt.setString(1, groupName);
 		        pstmt.setString(2, groupKeyword);
-		        pstmt.setString(3, name);
+		        pstmt.setString(3, loggedUsername);
 		        pstmt.executeUpdate();
 		 } catch (SQLException e) {
 			System.out.println(e.getMessage());
@@ -49,35 +49,76 @@ public class MsgGroups extends User {
 	}
 		
 		
-	public void addUsers(int id, String name) throws SQLException {
+	public void addUsersAfter(int id, String name) throws SQLException {
 		System.out.println("Please enter the username you would like"
 					 + "add to the Group.");
         	String username = input.nextLine();
 		if (checkExistingUser(name)) { 
-			String usersselect = "SELECT Username FROM Users WHERE Discoverable==true";
+			String usersselect = "SELECT Discoverable FROM Users WHERE Username ='"+name+"'";
 			try  {
 				Statement stmt  = conn.createStatement();
 				ResultSet queryresult = stmt.executeQuery(usersselect);
-		  		while (queryresult.next()) {
-		        		System.out.println("Username:" + queryresult.getString("Username") + "\t" + 
-					 	   	"Email:" +	queryresult.getString("email"));
-		 		}
+				if (queryresult.getInt(1) == 1) {
+					String sql = "INSERT INTO GroupUserRelations (RelUsername, RelMsgGroup)  "
+					      + "VALUES (?, ?)";
+					try {
+						PreparedStatement pstmt = conn.prepareStatement(sql);
+						pstmt.setString(1, username);
+						pstmt.setInt(2, id);
+						pstmt.executeUpdate();
+						System.out.println("User added successfully!");
+					} catch (SQLException e) {
+						System.err.println("Oops, somethign went wrong when adding the user!");
+					}
+				} else {
+					System.err.println("The user wants to remain private and can't get added to the group, sorry");
+				}
 			} catch (SQLException e) {
-				System.out.println(e.getMessage());
+				System.err.println("Oops, something went wrong!");
 			}
-				String sql = "INSERT INTO GroupUserRelations (RelUsername, RelMsgGroup)  "
-					      + "VALUES (username,id)";
-			try {
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, username);
-				pstmt.setInt(2, id);
-				pstmt.executeUpdate();
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-			}		
-			System.out.println("User added successfully!");
 		} else {
-	 	System.out.println("Username was not found, try again!");
+	 		System.out.println("Username was not found, try again!");
+		}
+	}
+	
+	public void addUsersDirectly(String name) throws SQLException {
+		System.out.println("Please enter the username you would like"
+					 + "add to the Group.");
+        	String username = input.nextLine();
+		while (true) {	
+			if (checkExistingUser(name)) { 
+				String usersselect = "SELECT Discoverable FROM Users WHERE Username ='"+name+"'";
+				try  {
+					Statement stmt  = conn.createStatement();
+					ResultSet queryresult = stmt.executeQuery(usersselect);
+					if (queryresult.getInt(1) == 1) {
+						String id = "SELECT MAX(MsgGroupID) 
+								FROM MsgGroups 
+								WHERE MsgGroupCreator='" + loggedUsername + "'"
+						String sql = "INSERT INTO GroupUserRelations (RelUsername, RelMsgGroup)  "
+						      + "VALUES (?, ?)";
+						try {
+							PreparedStatement pstmt = conn.prepareStatement(sql);
+							pstmt.setString(1, username);
+							pstmt.setInt(2, id);
+							pstmt.executeUpdate();
+							System.out.println("User added successfully!");
+							System.out.println("Want to add another user to the same Group? (Y/N)");
+							if (in.nextInt().equals("N")) {
+								break;
+							}
+						} catch (SQLException e) {
+							System.err.println("Oops, somethign went wrong when adding the user!");
+						}
+					} else {
+						System.err.println("The user wants to remain private and can't get added to the group, sorry");
+					}
+				} catch (SQLException e) {
+					System.err.println("Oops, something went wrong!");
+				}
+			} else {
+				System.out.println("Username was not found, try again!");
+			}
 		}
 	}
 }
