e18b88db18eb460c4fc817e6358b052bcc9a2ca5
HliasMpGH <115589434+HliasMpGH@users.noreply.github.com>
2022-12-18 16:01:58 +0200

    Updated Login

--- Progr2omadiki/src/main/java/core/Login.java
+++ Progr2omadiki/src/main/java/core/Login.java
@@ -7,25 +7,25 @@ import java.sql.Connection;
 
 
 public class UserLogin {
-    Scanner input = new Scanner(System.in);
-    public String login() {
-        String returned = "null";
+    public login() {
         String username="null";
         String password="null";
-        boolean gotoregister = false;
-        while (result.equals(null)) do {
+        while (result.equals(null)) {
             System.out.println("Please insert username:");
             username = input.nextline();
             System.out.println("Please insert Password:") ;
             password = input.nextline();
             
             if (checkExistingUser(username)){
-                ResultSet rs = stmt.executeQuery("SELECT * FROM Users WHERE Username="+username;);
+                ResultSet rs = stmt.executeQuery("SELECT * FROM Users WHERE Username='" + username + "'";);
                 String correctpw = rs.getString("Password");
                 if (correctpw == password){
                     System.out.println("Successful Login!");
-                    returned = rs.getString("Username");
-                    //allagh last login time
+                    loggedUsername = rs.getString("UserName");                    
+                    String sql = "UPDATE Users 
+                                  SET LastLoginTime = DATETIME('now','localtime')) 
+                                  WHERE Username='" + loggedUsername + "'";
+                    stmt.updateQuery(sql);
                 }
             } else {
                 int a;
@@ -40,7 +40,7 @@ public class UserLogin {
                     continue;
                 }
             }
-            return returned;
+            
         }
     }
 }
