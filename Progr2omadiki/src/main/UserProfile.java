89701cf5a9efa0c40b9f00961abd8352dace2fcf
StefanKourou <115430541+StefanKourou@users.noreply.github.com>
2023-01-10 20:47:38 +0200

    Javadoc changes

--- Progr2omadiki/src/main/UserProfile.java
+++ Progr2omadiki/src/main/UserProfile.java
@@ -14,31 +14,31 @@ public class UserProfile {
     static Scanner input = new Scanner(System.in);
 
     /**
-    * Connection object 
+    * Connection object.
     */
     static Connection conn;
 
     /**
-    * logged-in user, all methods are responsible for retrieving data from this user
+    * Logged-in user, all methods are responsible for retrieving data from this user.
     */
     static String loggedUsername;
 
     /**
-    *  Sets the Connection object
+    *  Sets the Connection object.
     */
     public static void setConn(Connection conne) {
         conn = conne;
     }
 
     /** 
-    *  Sets the logged-in user
+    *  Sets the logged-in user.
     */
     public static void setLogName(String name) {
         loggedUsername = name;
     }
 
     /**
-     * shows to the screen a complete profile of the logged-in user
+     * Shows to the screen a complete profile of the logged-in user.
      */
     public static void showProfile() {
         String sql1 = "SELECT Email, Discoverable, UserKeywords " +
@@ -74,7 +74,7 @@ public class UserProfile {
     }
 
     /**  
-    *  Shows the full stats of the user
+    *  Shows the full stats of the user.
     */
     public static void showFullStats() {
         showMsgCount();
@@ -83,7 +83,7 @@ public class UserProfile {
     }
 
     /**  
-    *  Shows the Count of the Messages sent by the user
+    *  Shows the Count of the Messages sent by the user.
     */
     public static void showMsgCount() {
         int msgs;
@@ -100,7 +100,7 @@ public class UserProfile {
     }
 
     /**  
-    *  Shows the current Login time of the user
+    *  Shows the current Login time of the user.
     */
     public static void showTimeLoggedIn() {
         String timeLogged;
@@ -117,7 +117,7 @@ public class UserProfile {
     }
 
     /**  
-    *  Shows the Reactions that the user has given and has been given with a graphical way
+    *  Shows the Reactions that the user has given and received in a graphical way.
     */
     public static void showReactions() {
         int likesUserGave = 0;
@@ -190,12 +190,12 @@ public class UserProfile {
                                     "disliked " + dislikesUserHas + " time(s)" + "\n\t" +
                                     "and loved " + heartsUserHas + " time(s)");
         } catch(SQLException e) {
-            System.err.print("Opps! Something Went Wrong While Retrieving Data!");
+            System.err.print("Oops! Something went wrong while retrieving data!");
         }  
     }
 
     /**  
-    *  Returns the Last Login time of the user
+    *  Returns the last Login time of the user.
     */
     public static String getLoginTime() {
         String logintime;
@@ -206,14 +206,14 @@ public class UserProfile {
             ResultSet rs = stm.executeQuery(sql)) {
                 logintime = rs.getString("LastLoginTime");
         } catch (SQLException e) {
-            System.err.print("Opps! Something Went Wrong While Retrieving Data!");
+            System.err.print("Oops! Something went wrong while retrieving data!");
             logintime = "-1";
         }              
         return logintime;
     }
 
     /**  
-    *  Returns the Last Logout time of the user
+    *  Returns the last Logout time of the user.
     */
     public static String getLogoutTime() {
         String logouttime;
