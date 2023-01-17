4398c12bc47c6de3c5140da6ccd73f10482596a1
HliasMpGH <t8210226@aueb.gr>
2023-01-18 00:15:28 +0200

    Added Classes again

--- Progr2omadiki/src/main/Main.java
+++ Progr2omadiki/src/main/Main.java
@@ -16,75 +16,74 @@ import java.util.Scanner;
  */
 
 public class Main {
-	/**
-	 * Establishes a connection to the Progr2 Database.
-	 */
-	public static Connection connect() {
-		Connection conn = null;
-		try {
-			// db parameters
-			String url = "jdbc:sqlite:C:/sqlite/db/Progr2.db";
-			// create a connection to the database
-			conn = DriverManager.getConnection(url);
+    /**
+     * Establishes a connection to the Progr2 Database.
+     * @return a Connection object with the Progr2 Database signature
+     */
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/sqlite/db/Progr2.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
 
-			System.out.println("Connection to SQLite has been established.");
-
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		return conn;
-	}
-
-	/**
-	 * @param args the command line arguments Contains the main screen of the
-	 *             program
-	 */
-	public static void main(String[] args) {
-		boolean end = false;
-		Scanner input = new Scanner(System.in);
-		Connection conn = connect();
-		Message obj = new Message(conn);
-		obj.clearScreen();
-		System.out.println("---Welcome To UniTed---");
-		do {
-			try {
-				Thread.sleep(1050);
-				obj.clearScreen();
-			} catch (InterruptedException e) {
-			}
-			System.out.println("Press 1 to Sign in");
-			System.out.println("Press 2 to Sign up");
-			System.out.println("Press 3 to Exit The App (sorry to see you go...)");
-			int ans;
-			do {
-				ans = input.nextInt();
-				obj.clearScreen();
-				switch (ans) {
-				case 1:
-					obj.login();
-					break;
-				case 2:
-					obj.registerNewUser();
-					obj.login();
-					break;
-				case 3:
-					end = true; // user wants to exit the app
-					System.out.println("bye...");
-					try {
-						Thread.sleep(1000);
-						obj.killScreen();
-					} catch (InterruptedException e) {
-					}
-					break;
-				default:
-					obj.clearScreen();
-					System.out.println("Invalid input! please type 1, 2 or 3");
-					continue;
-				}
-				break;
-			} while (true);
-		} while (!end);
-		input.close();
-	}
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+        
+    /**
+     * @param args the command line arguments
+     * Contains the main screen of the program
+     */
+    public static void main(String[] args) {
+        boolean end = false;
+        Scanner input = new Scanner(System.in);
+        Connection conn = connect();
+        Message obj = new Message(conn);
+        obj.clearScreen();
+        System.out.println("---Welcome To UniTed---");
+        do {
+            try {
+                Thread.sleep(1050);
+                obj.clearScreen();
+            } catch (InterruptedException e) {}
+            System.out.println("Press 1 to Sign in");
+            System.out.println("Press 2 to Sign up");
+            System.out.println("Press 3 to Exit The App (sorry to see you go...)");
+            int ans;
+            do {
+                ans = input.nextInt();
+                obj.clearScreen();
+                switch (ans) {
+                    case 1:
+                        obj.login();
+                        break;
+                    case 2:
+                        obj.registerNewUser();
+                        obj.login();
+                        break;
+                    case 3:
+                        end = true; // user wants to exit the app
+                        System.out.println("bye...");
+                        try {
+                            Thread.sleep(1000);
+                            obj.killScreen();
+                        } catch (InterruptedException e){}
+                        break;
+                    default:
+                        obj.clearScreen();
+                        System.out.println("Invalid input! please type 1, 2 or 3");
+                        continue;
+                }
+                break;
+            } while (true);
+        } while (!end);
+        input.close();
+    }
 
 }
--- Progr2omadiki/src/main/Message.java
+++ Progr2omadiki/src/main/Message.java
@@ -14,34 +14,34 @@ import java.sql.Connection;
 
 public class Message extends MsgGroup {
 
-	/**
-	 * The Group that the user is in right now. It's being set by the
-	 * showLastMessages(int) method
+	/** 
+	 * The Group that the user is in right now.
+	 * It's being set by the showLastMessages(int) method
 	 */
-	int groupID;
+    int groupID; 
 
-	/**
+	/** 
 	 * Passes the Connection object to the super-class.
+	 * @param conn a Connection object with a Database signature
 	 */
+    public Message(Connection conn) {
+        super(conn);
+    }
 
-	public Message(Connection conn) {
-		super(conn);
-	}
-
-	/**
+	/**  
 	 * shows the last 10 Messages of a group
-	 * 
 	 * @param groupID the groupID of the said group
-	 */
-
+	*/
 	public void showLastMessages(int groupID) {
 		in.nextLine(); // clear the buffer
 		this.groupID = groupID;
 		int n = 0; // used to parameterize the ShowMsgs query to show more msgs
 		clearScreen();
 		// check if this group has any messages
-		String sqltest = "SELECT COUNT(MsgText) " + "FROM Messages " + "WHERE MsgGroup = " + groupID;
-		try (Statement stmt = conn.createStatement();) {
+		String sqltest = "SELECT COUNT(MsgText) " +
+						"FROM Messages " +
+						"WHERE MsgGroup = " + groupID;
+	    try (Statement stmt  = conn.createStatement();) {
 			System.out.print("Opening Group");
 			Thread.sleep(500);
 			System.out.print(".");
@@ -51,7 +51,9 @@ public class Message extends MsgGroup {
 			System.out.print(".");
 			Thread.sleep(500);
 			do {
-				String groupName = "SELECT MsgGroupName " + "FROM MsgGroups " + "WHERE msgGroupID = " + groupID;
+				String groupName = "SELECT MsgGroupName " +
+									"FROM MsgGroups " +
+									"WHERE msgGroupID = " + groupID;
 				ResultSet rs = stmt.executeQuery(groupName);
 				clearScreen();
 				System.out.println("------- " + rs.getString("MsgGroupName") + " -------");
@@ -59,29 +61,31 @@ public class Message extends MsgGroup {
 				int msgCount;
 				// if this group has at least 1 message, show the last 10 on the screen
 				if ((msgCount = rs.getInt(1)) > 0) {
-					String msgs = "SELECT MsgText, MsgUsername, MsgID "
-							+ "FROM (SELECT MsgText, MsgUsername, MsgCreationTime, MsgID " + "FROM Messages "
-							+ "WHERE MsgGroup = " + groupID + " " + "ORDER BY MsgCreationTime DESC LIMIT 10 OFFSET " + n
-							+ ")" + "ORDER BY MsgCreationTime ASC";
+					String msgs = "SELECT MsgText, MsgUsername, MsgID " +
+									"FROM (SELECT MsgText, MsgUsername, MsgCreationTime, MsgID " +
+											"FROM Messages " +
+											"WHERE MsgGroup = " + groupID + " " +
+											"ORDER BY MsgCreationTime DESC LIMIT 10 OFFSET " + n + ")" +
+									"ORDER BY MsgCreationTime ASC";
 					rs = stmt.executeQuery(msgs);
 					// loop through the result set
 					while (rs.next()) {
-						System.out.println(rs.getString("MsgUsername") + "\t" + "(" + rs.getInt("MsgID") + ")"
-								+ rs.getString("MsgText") + "\t");
+						System.out.println(rs.getString("MsgUsername") + "\t" +
+												"(" + rs.getInt("MsgID") + ")" +
+												rs.getString("MsgText") + "\t");
 					}
-				} else {
+				}else {
 					System.out.println("This Group doesn't have any Messages. Total silence....");
 				}
 				System.out.println("< and > for more messages");
 				Thread.sleep(1000);
-				System.out.println(
-						"\n" + "type NEW if you want to send a new message, SEL if you wish to select a message, \n"
-								+ "or MEM to see its members");
-
+				System.out.println("\n" + "type NEW if you want to send a new message, SEL if you wish to select a message, \n" +
+										"or MEM to see its members");
+			
 				System.out.println("type NO to return to main screen");
-				String ans = in.nextLine();
-				while (!ans.equals("NEW") && !ans.equals("NO") && !ans.equals("SEL") && !ans.equals("<")
-						&& !ans.equals(">") && !ans.equals("MEM")) {
+				var ans = in.nextLine();
+				while (!ans.equals("NEW") && !ans.equals("NO") && !ans.equals("SEL") && 
+						 !ans.equals("<") && !ans.equals(">") && !ans.equals("MEM")) {
 
 					System.out.println("Invalid input, please type NEW, SEL, MEM or NO ");
 					ans = in.nextLine();
@@ -89,14 +93,13 @@ public class Message extends MsgGroup {
 				if (ans.equals("NEW")) {
 					clearScreen();
 					System.out.print("Type your nice words: ");
-					String text = in.nextLine();
+					var text = in.nextLine();
 					createMessage(text);
 					n = 0; // go to the bottom of the chat
-					continue; // after sending the msg, show again all the group msgs(including the one just
-								// send)
+					continue; // after sending the msg, show again all the group msgs(including the one just send)
 				} else if (ans.equals("SEL")) {
 					System.out.print("Give the ID of the message you want to select: ");
-					int msgID = in.nextInt();
+					var msgID = in.nextInt();
 					showMessageInfo(msgID);
 					n = 0; // go to the bottom of the chat
 					continue; // after selecting the msg, show again all the group msgs
@@ -126,125 +129,126 @@ public class Message extends MsgGroup {
 				}
 				break;
 			} while (true);
-		} catch (SQLException e) {
+	    } catch (SQLException e) {
 			System.err.println("Something went wrong when accessing the messages from this group!");
-		} catch (InterruptedException e) {
-		}
-	}
+	    } catch (InterruptedException e) {}
+    }
 
-	/**
+	/**  
 	 * Creates a new Message for a Group.
-	 * 
 	 * @param msgText the Message that will be sent to the group
-	 */
-	public void createMessage(String msgText) {
-		String newMessage = "INSERT INTO Messages (" + "MsgUsername, MsgGroup, MsgText, MsgCreationTime) " + "VALUES ("
-				+ "'" + loggedUsername + "'" + "," + groupID + ",'" + msgText + "', DATETIME('now','localtime'))";
-		try (Statement stmt = conn.createStatement();) {
+	*/
+    public void createMessage(String msgText) {
+		String newMessage =
+		"INSERT INTO Messages (" +
+		    "MsgUsername, MsgGroup, MsgText, MsgCreationTime) " +
+			"VALUES (" +			
+			"'" + loggedUsername + "'" + "," + groupID + ",'" + msgText + "', DATETIME('now','localtime'))";
+		try (Statement stmt  = conn.createStatement();) {
 			stmt.executeUpdate(newMessage);
 		} catch (SQLException e) {
 			System.err.println("Oops! Something went wrong while sending the message!");
 		}
-	}
-
-	/**
+    }
+	
+	/**  
 	 * Shows info about a specific Message, including the MessageCreationTime.
-	 * 
 	 * @param msgID the ID of the message that we want to see information about
-	 */
-
+	*/
 	public void showMessageInfo(int msgID) {
 		clearScreen();
 		// check if this group has any msgs with the id of msgID
-		String msgInfoTest = "SELECT COUNT(MsgText) " + "FROM Messages where MsgID =" + msgID + " " + "AND MsgGroup = "
-				+ groupID;
-		try (Statement stmt = conn.createStatement();) {
+	    String msgInfoTest = "SELECT COUNT(MsgText) " +
+								"FROM Messages where MsgID =" + msgID + " " + 
+								"AND MsgGroup = " + groupID;
+        try (Statement stmt  = conn.createStatement();) {
 			ResultSet queryresult = stmt.executeQuery(msgInfoTest);
-			// if this group has at least 1 message matching the criteria, show the msgs to
-			// the screen
+			// if this group has at least 1 message matching the criteria, show the msgs to the screen
 			if (queryresult.getInt(1) > 0) {
-				String msgInfo = "SELECT MsgUsername, MsgText, MsgCreationTime " + "FROM Messages where MsgID =" + msgID
-						+ " " + "AND MsgGroup = " + groupID;
+				String msgInfo = "SELECT MsgUsername, MsgText, MsgCreationTime " +
+									"FROM Messages where MsgID =" + msgID + " " + 
+									"AND MsgGroup = " + groupID;
 				queryresult = stmt.executeQuery(msgInfo);
-				System.out.println("The user who sent it was: " + queryresult.getString("MsgUsername") + "\n"
-						+ "The message was: '" + queryresult.getString("MsgText") + "'\n"
-						+ "This message was created at: " + queryresult.getString("MsgCreationTime") + "\n");
+				System.out.println("The user who sent it was: " + queryresult.getString("MsgUsername") + "\n" + 
+						"The message was: '" + queryresult.getString("MsgText") + "'\n" +
+						"This message was created at: " + queryresult.getString("MsgCreationTime") + "\n");
 				System.out.println("Do you wish to react to this message?");
 				System.out.println("1: like, 2: dislike, 3: heart");
 				System.out.println("4: go back");
 				int re = in.nextInt();
-				while (re != 1 && re != 2 && re != 3 && re != 4) {
+				while (re != 1 && re != 2 && re != 3 && re != 4){
 					clearScreen();
 					System.out.println("Invalid input, please type 1, 2, 3 or 4");
 					System.out.println("1: like, 2: dislike, 3: heart");
 					System.out.println("4: go back");
 					re = in.nextInt();
 				}
-				if (re != 4)
-					reactionsMessage(msgID, re);
+				if (re != 4) reactionsMessage(msgID, re);
 			} else {
 				System.out.println("No Messages Found with the ID of " + msgID);
 				Thread.sleep(1500);
 			}
-		} catch (SQLException e) {
-			System.err.println("Oops! Something went wrong when retrieving the message.");
+        } catch (SQLException e) {
+            System.err.println("Oops! Something went wrong when retrieving the message.");
 		} catch (InterruptedException e) {
-		} finally {
+        } finally {
 			in.nextLine(); // clear the buffer
 		}
-	}
+    }
 
-	/**
+	/**  
 	 * Registers a reaction to a Message
-	 * 
-	 * @param msgID the ID of the Message and re the reaction
-	 */
-
-	public void reactionsMessage(int msgID, int re) {
-		String newReaction = "INSERT INTO Reactions (ReMsg, ReUsername, Reaction)" + "VALUES (" + msgID + ",'"
-				+ loggedUsername + "'," + re + ")";
-		try (Statement stmt = conn.createStatement()) {
+	 * @param msgID the ID of the Message
+	 * @param re the reaction
+	*/
+    public void reactionsMessage(int msgID, int re) {
+        String newReaction =
+        "INSERT INTO Reactions (ReMsg, ReUsername, Reaction)" +
+        "VALUES (" + msgID + ",'" + loggedUsername + "'," + re + ")";
+		try (Statement stmt  = conn.createStatement()) {
 			stmt.executeUpdate(newReaction);
 		} catch (SQLException e) {
 			clearScreen();
 			try {
-				System.err.println(
-						"Oops! Something went wrong when reacting to the message (maybe you tried to react to the same message twice?)");
-				Thread.sleep(2000);
-			} catch (InterruptedException ex) {
-			}
+				System.err.println("Oops! Something went wrong when reacting to the message (maybe you tried to react to the same message twice?)");	
+				Thread.sleep(2000);	
+			} catch (InterruptedException ex) {}
 		}
-	}
+    }
 
 	/**
 	 * Shows to the screen the members of the selected group.
 	 */
 	public void showGroupMembers() {
 		// query to find the owner of the group
-		String findOwnerOfGroup = "SELECT MG.MsgGroupCreator " + "FROM MsgGroups MG " + "WHERE MG.MsgGroupID ="
-				+ groupID;
-		// query to find the members of the group
-		String findMembersOfGroup = "SELECT GU.RelUsername as members " + "FROM GroupUsersRelations GU "
-				+ "WHERE GU.RelMsgGroup =" + groupID + " " + "EXCEPT " + "SELECT MG.MsgGroupCreator "
-				+ "FROM MsgGroups MG " + "WHERE MG.MsgGroupID =" + groupID;
+		String findOwnerOfGroup = "SELECT MG.MsgGroupCreator " +
+									"FROM MsgGroups MG " +
+									"WHERE MG.MsgGroupID =" + groupID;
+		// query to find the members of the group							
+		String findMembersOfGroup = "SELECT GU.RelUsername as members " +
+									"FROM GroupUsersRelations GU " +
+									"WHERE GU.RelMsgGroup =" + groupID + " " +
+									"EXCEPT " +
+									"SELECT MG.MsgGroupCreator " +
+									"FROM MsgGroups MG " +
+									"WHERE MG.MsgGroupID =" + groupID;		
 		try (Statement stmt = conn.createStatement();) {
 			ResultSet rs = stmt.executeQuery(findOwnerOfGroup);
 			String ownerOfGroup = rs.getString(1);
-			System.out.println("OWNER: " + ownerOfGroup); // show the owner to the screen
+			System.out.println( "OWNER: " + ownerOfGroup); // show the owner to the screen
 			rs = stmt.executeQuery(findMembersOfGroup);
 			System.out.println("MEMBERS: ");
 			// show the members to the screen
 			while (rs.next()) {
 				System.out.println(rs.getString("members"));
-			}
+			}	
 			System.out.println("\nDo you want to add a user to the chat? (YES/NO)");
 			String ans = in.nextLine();
 			while (!ans.equals("YES") && !ans.equals("NO")) {
 				System.out.println("Invalid Input! Please type YES or NO");
 				ans = in.nextLine();
 			}
-			if (ans.equals("YES"))
-				addUser(0); // give 0 as an parameter so it can know to calculate the groupID properly
+			if (ans.equals("YES")) addUser(0); // give 0 as an parameter so it can know to calculate the groupID properly
 			rs.close();
 		} catch (SQLException e) {
 			System.err.println("Something went wrong while showing the members!");
